380edc5a531d28087a5e178b6a717cb8b7148a43
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-06-28 16:05:34 +0300

    Search input tu use it in fltering

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@ import { ref } from 'vue'
 import Notes from './components/Notes.vue'
 import AddNoteBtn from './components/AddNoteBtn.vue'
 import Popup from './components/AddNotesPopup.vue'
+import Search from './components/Search.vue'
 
 interface AnyObject {
   [key: string]: any
 }
+
+const query = ref<string>()
+
 interface Triggers extends AnyObject {
   buttonTrigger: boolean
   timedTrigger: boolean
@@ -30,15 +34,15 @@ const togglePopup = (trigger: string, state?: boolean) => {
 
 <template>
   <div class="container mx-auto h-screen max-w-3xl relative py-6">
-    <header class="w-full">
+    <header class="w-full mx-3">
       <h1 class="text-2xl xl:text3xl 2xl:text-4xl font-bold my-2">My Notes</h1>
-      <div class="w-full flex justify-between">
-        <Search />
+      <div class="w-full flex flex-row justify-between relative px-3">
+        <Search :query="query || ''" />
         <AddNoteBtn :toggle="togglePopup" />
       </div>
     </header>
     <section
-      class="w-full h-[90%] mt-6 overflow-y-scroll scrollbar-thin scrollbar-track-violet-500/20 scrollbar-corner-thumb-purple-500/20 scrollbar-thumb-rounded-md lg:scrollbar-thumb-violet-500/90 scrollbar-track-rounded-full lg:scroll-smooth"
+      class="w-full h-[80%] lg:h-[85%] xl:h-[89%] mt-6 overflow-y-scroll scrollbar-thin scrollbar-track-violet-500/20 scrollbar-corner-thumb-purple-500/20 scrollbar-thumb-rounded-md lg:scrollbar-thumb-violet-500/90 scrollbar-track-rounded-full lg:scroll-smooth"
     >
       <Notes />
     </section>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,28 @@
-<template>
-  <div>
-    <input type="text" class="" placeholder="Search..." />
-  </div>
-</template>
 <script lang="ts" setup>
-const props = defineProps<string>()
+import Search from '../assets/search-normal.svg'
+
+interface Props {
+  query: string
+}
+
+const props = defineProps<Props>()
 console.log('props', props)
 </script>
+
+<template>
+  <div
+    class="w-full flex relative xl:fixed xl:right xl:w-fit top-[3%] xl:trasform xl:translate-x-[75%] xl:left-1/2"
+  >
+    <input
+      type="text"
+      class="bg-white/10 font-normal text-base rounded-sm px-2 placeholder-gray-400 w-full py-2 mt-2"
+      placeholder="Search..."
+    />
+    <img
+      :src="Search"
+      alt="search"
+      srcset=""
+      class="h-5-w-5 absolute right-5 transform translate-y-[50%] bottom-1/2 top-1 opacity-60"
+    />
+  </div>
+</template>
