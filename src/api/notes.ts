521c59af75cc6d4c996dcef400d1e8e58622b41f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-06-29 13:35:15 +0300

    notes changes

--- src/api/notes.ts
+++ src/api/notes.ts
@@ -1,35 +1,56 @@
-import { PostANote, Note } from '../type'
-
+import { PostANote, Note, DataError } from '../type'
 const headers = new Headers({ 'Content-Type': 'application/json' })
 const Url = import.meta.env.API_URL || 'http://127.0.0.1:8000/api'
 console.log('Url', Url)
 
 // get the all notes
-export async function getAllNotes(): Promise<Note[]> {
-  const response = await fetch(`${Url}/notes`, {
-    headers,
-  })
-  return await response.json()
+export async function getAllNotes(): Promise<Note[] | undefined> {
+  try {
+    const response = await fetch(`${Url}/notes`, {
+      headers,
+    })
+    const data = await response.json()
+    if (response.status !== 200) {
+      throw data.error
+    }
+    return data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 // post a note
 export async function postANote(note: PostANote) {
   console.log('note', note)
-  const response = await fetch(`${Url}/notes`, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(note),
-  })
-  const data = await response.json()
-  return data
+  try {
+    const response = await fetch(`${Url}/notes`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(note),
+    })
+    const data = await response.json()
+    if (response.status !== 200) {
+      throw data.error as DataError
+    }
+    return data
+  } catch (error) {
+    console.error('data error', error)
+  }
 }
 
 //delete a note
 export async function deleteANote(id: number) {
-  const response = await fetch(`${Url}/notes/${id}`, {
-    method: 'DELETE',
-    headers,
-  })
-  const data = await response.json()
-  return data
+  try {
+    const response = await fetch(`${Url}/notes/${id}`, {
+      method: 'DELETE',
+      headers,
+    })
+    const data = await response.json()
+    if (response.status !== 200) {
+      throw data.error
+    }
+    return data
+  } catch (error) {
+    console.error(error)
+  }
 }
